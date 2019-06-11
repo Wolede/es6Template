d4d33d16408121fc0afba9e9b6301f8b9d52c03b
wolede <oluwawolede.adeniyi@venturegardengroup.com>
2019-06-11 19:33:32 +0100

    added react to the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import Entity from "./entity";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-class Hobbit extends Entity{
-    constructor(name, height) {
-        super(name, height);
+  const url = "https://edutech-cors-anywhere.herokuapp.com/https://canvas.abudlc.edu.ng";
+  const api = `${url}/api/v1/`;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            courseCode: ''
+        }
     }
 
-    greet() {
-        console.log(`Hello! I'm ${this.name} from Naija`);
+    componentDidMount() {
+        this.getCourses()
+    }
+    getCourses() {
+        fetch(`${api}courses?enrollment_type=student&access_token=${token}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({name: data[0].name, courseCode: data[0].course_code})
+            console.log(data[0].name)
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Name: {this.state.name}</h1>
+                <h2>Course Code: {this.state.courseCode}</h2>
+            </div>
+        );
     }
 }
 
-let Frodo = new Hobbit("Frodo Baggins", 4.5);
-Frodo.greet();
+ReactDOM.render(<App />, document.getElementById('root'));
